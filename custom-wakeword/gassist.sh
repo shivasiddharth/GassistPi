2a1eb89ceb8855bab581fef2a95095629285da86
shivasiddharth <ushivasiddharth@gmail.com>
2017-07-08 13:11:36 +0100

    Update gassist.sh

--- custom-wakeword/gassist.sh
+++ custom-wakeword/gassist.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-/home/pi/env/bin/python3 -u /home/pi/GassistPi/google-wakeword/main.py
+/home/pi/env/bin/python3 -u /home/pi/GassistPi/custom-wakeword/main.py
+/home/pi/env/bin/python3 -u /home/pi/GassistPi/custom-wakeword/snowboy/wakeword.py custom.pmdl
