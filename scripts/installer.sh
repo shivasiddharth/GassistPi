14d2a662b3dd45fa7d8e07e26f6903553d0a30f9
shivasiddharth <ushivasiddharth@gmail.com>
2017-09-09 13:16:19 +0100

    Create installer.sh

---
+++ scripts/installer.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# Copyright 2017 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+
+scripts_dir="$(dirname "${BASH_SOURCE[0]}")"
+
+# make sure we're running as the owner of the checkout directory
+RUN_AS="$(ls -ld "$scripts_dir" | awk 'NR==1 {print $3}')"
+if [ "$USER" != "$RUN_AS" ]
+then
+    echo "This script must run as $RUN_AS, trying to change user..."
+    exec sudo -u $RUN_AS $0
+fi
+
+sudo apt-get update  
+sudo pat-get install raspberrypi-kernel  
+sudo apt-get install python3-dev python3-venv  
+python3 -m venv env  
+env/bin/python -m pip install --upgrade pip setuptools  
+source env/bin/activate  
+sudo apt-get install portaudio19-dev libffi-dev libssl-dev -y  
+sudo apt-get install swig3.0 python-pyaudio python3-pyaudio sox -y  
+pip install pyaudio  
+sudo apt-get install libatlas-base-dev -y  
+pip install RPi.GPIO  
+python -m pip install --upgrade google-assistant-library  
+python -m pip install --upgrade google-auth-oauthlib[tool] 
+google-oauthlib-tool --client-secrets /home/pi/assistant.json --scope https://www.googleapis.com/auth/assistant-sdk-prototype --save --headless    
