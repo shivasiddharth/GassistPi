123663ff221c4b440e2c2b0e8c780ab91a969c94
shivasiddharth <ushivasiddharth@gmail.com>
2017-11-07 09:32:58 +0000

    Update gassist-installer-pi3.sh

--- scripts/gassist-installer-pi3.sh
+++ scripts/gassist-installer-pi3.sh
@@ -28,6 +28,7 @@ fi
 cd /home/pi/
 sudo pip3 install mps-youtube youtube-dl 
 sudo apt-get install vlc -y
+sudo apt-get install chromium-browser -y
 mpsyt set player chromium-browser, set playerargs ,exit
 sudo apt-get install elinks -y
 sudo apt-get update -y
