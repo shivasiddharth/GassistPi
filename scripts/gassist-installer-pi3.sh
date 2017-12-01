798b9ed2ca4c93944520dee0c2f8cb677149f1a6
shivasiddharth <ushivasiddharth@gmail.com>
2017-12-01 17:10:24 +0000

    Update gassist-installer-pi3.sh

--- scripts/gassist-installer-pi3.sh
+++ scripts/gassist-installer-pi3.sh
@@ -33,11 +33,13 @@ sudo apt-get install elinks -y
 sudo apt-get update -y
 sudo apt-get install python3-dev python3-venv -y
 sudo apt-get install portaudio19-dev libffi-dev libssl-dev -y
+sudo apt-get install libttspico0 libttspico-utils libttspico-data -y
 python3 -m venv env
 env/bin/python -m pip install --upgrade pip setuptools
 source env/bin/activate
 pip install RPi.GPIO
 pip install pyaudio
+pip install aftership
 python -m pip install --upgrade google-assistant-library
 python -m pip install --upgrade google-assistant-sdk
 python -m pip install --upgrade google-assistant-sdk[samples]
