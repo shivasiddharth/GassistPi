bddd9a988c73c1bee0ba7373815170feea77ba33
shivasiddharth <ushivasiddharth@gmail.com>
2017-12-01 15:28:05 +0000

    Update actions.py

--- src/actions.py
+++ src/actions.py
@@ -101,11 +101,22 @@ def track():
     text=api.trackings.get(tracking=dict(slug=slug, tracking_number=number))
     numtrack=len(text['trackings'])
     print("Total Number of Parcels: " + str(numtrack))
+    if numtrack==0:
+        parcelnotify=("You have no parcel to track")
+        say(parcelnotify)
+    elif numtrack==1:
+        parcelnotify=("You have one parcel to track")
+        say(parcelnotify)
+    elif numtrack>1:
+        parcelnotify=( "You have " + str(numtrack) + " parcels to track")
+        say(parcelnotify)
     for x in range(0,numtrack):
         numcheck=len(text[ 'trackings'][x]['checkpoints'])
         description = text['trackings'][x]['checkpoints'][numcheck-1]['message']
         parcelid=text['trackings'][x]['tracking_number']
         trackinfo= ("Parcel Number " + str(x+1)+ " with tracking id " + parcelid + " is "+ description)
+        say(trackinfo)
+        time.sleep(10)
 
 #GPIO Device Control
 def Action(phrase):
