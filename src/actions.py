761369867cf520576ff4765f7abc1116da4f90f1
shivasiddharth <ushivasiddharth@gmail.com>
2017-12-15 00:09:08 +0000

    Kodi-Youtube
    Testing playing of youtube videos in kodi

--- src/actions.py
+++ src/actions.py
@@ -3,7 +3,9 @@
 #This is different from AIY Kit's actions
 #Copying and Pasting AIY Kit's actions commands will not work
 
-from kodijson import Kodi
+from kodijson import Kodi, PLAYER_VIDEO
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 import os
 import os.path
 import RPi.GPIO as GPIO
@@ -12,6 +14,18 @@ import re
 import subprocess
 import aftership
 import feedparser
+
+#YouTube API Constants
+DEVELOPER_KEY = 'AIzaSyALGk9d83bEHg_Nnn5VP-AH53aI0D3Crbc'
+YOUTUBE_API_SERVICE_NAME = 'youtube'
+YOUTUBE_API_VERSION = 'v3'
+
+#Login with default kodi/kodi credentials
+#kodi = Kodi("http://localhost:8080/jsonrpc")
+
+#Login with custom credentials
+# Kodi("http://IP-ADDRESS-OF-KODI:8080/jsonrpc", "username", "password")
+kodi = Kodi("http://localhost:8080/jsonrpc", "kodi", "kodi")
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -63,6 +77,70 @@ technews = "http://feeds.bbci.co.uk/news/technology/rss.xml"
 topnews = "http://feeds.bbci.co.uk/news/rss.xml"
 sportsnews = "http://feeds.feedburner.com/ndtvsports-latest"
 quote = "http://feeds.feedburner.com/brainyquote/QUOTEBR"
+
+
+#Function to search YouTube and get videoid
+def youtube_search(query):
+  youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+    developerKey=DEVELOPER_KEY)
+
+  req=query
+  # Call the search.list method to retrieve results matching the specified
+  # query term.
+  search_response = youtube.search().list(
+    q=query,
+    part='id,snippet'
+  ).execute()
+
+  videos = []
+  channels = []
+  playlists = []
+  videoids = []
+  channelids = []
+  playlistids = []
+
+  # Add each result to the appropriate list, and then display the lists of
+  # matching videos, channels, and playlists.
+
+  for search_result in search_response.get('items', []):
+                  
+    if search_result['id']['kind'] == 'youtube#video':
+      videos.append('%s (%s)' % (search_result['snippet']['title'],
+                                 search_result['id']['videoId']))
+      videoids.append(search_result['id']['videoId'])
+
+    elif search_result['id']['kind'] == 'youtube#channel':
+      channels.append('%s (%s)' % (search_result['snippet']['title'],
+                                   search_result['id']['channelId']))
+      channelids.append(search_result['id']['channelId'])
+
+    elif search_result['id']['kind'] == 'youtube#playlist':
+      playlists.append('%s (%s)' % (search_result['snippet']['title'],
+                                    search_result['id']['playlistId']))
+      playlistids.append(search_result['id']['playlistId'])
+
+  #Results of YouTube search. If you wish to see the results, uncomment them
+  # print 'Videos:\n', '\n'.join(videos), '\n'
+  # print 'Channels:\n', '\n'.join(channels), '\n'
+  # print 'Playlists:\n', '\n'.join(playlists), '\n'
+
+  #Checks if your query is for a channel, playlist or a video and changes the URL accordingly
+  if 'channel'.lower() in  str(req).lower() and len(channels)!=0:
+      urlid=channelids[0]
+      YouTubeURL=("https://www.youtube.com/watch?v="+channelids[0])
+  elif 'playlist'.lower() in  str(req).lower() and len(playlists)!=0:
+      urlid=playlistids[0]
+      YouTubeURL=("https://www.youtube.com/watch?v="+playlistids[0])
+  else:
+      urlid=videoids[0]
+      YouTubeURL=("https://www.youtube.com/watch?v="+videoids[0])
+
+ #If you want to see the URL, uncomment the following line
+ #print(YouTubeURL)
+
+ #Instead of sending it to Kodi, if you want to locally play in VLC, uncomment the following line and comment the second line
+ #os.system("vlc "+YouTubeURL)
+  kodi.Player.open(item={"file":"plugin://plugin.video.youtube/?action=play_video&videoid=" + urlid})
 
 
 #Text to speech converter
@@ -184,7 +262,7 @@ def feed(phrase):
         else:
             continue
 
-    
+
 
 #GPIO Device Control
 def Action(phrase):
