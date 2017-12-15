4f04e0bf970530de9930bc5f69760b93fb1e4d93
shivasiddharth <ushivasiddharth@gmail.com>
2017-12-15 09:49:19 +0000

    Update actions.py

--- src/actions.py
+++ src/actions.py
@@ -79,70 +79,6 @@ sportsnews = "http://feeds.feedburner.com/ndtvsports-latest"
 quote = "http://feeds.feedburner.com/brainyquote/QUOTEBR"
 
 
-#Function to search YouTube and get videoid
-def youtube_search(query):
-  youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
-    developerKey=DEVELOPER_KEY)
-
-  req=query
-  # Call the search.list method to retrieve results matching the specified
-  # query term.
-  search_response = youtube.search().list(
-    q=query,
-    part='id,snippet'
-  ).execute()
-
-  videos = []
-  channels = []
-  playlists = []
-  videoids = []
-  channelids = []
-  playlistids = []
-
-  # Add each result to the appropriate list, and then display the lists of
-  # matching videos, channels, and playlists.
-
-  for search_result in search_response.get('items', []):
-                  
-    if search_result['id']['kind'] == 'youtube#video':
-      videos.append('%s (%s)' % (search_result['snippet']['title'],
-                                 search_result['id']['videoId']))
-      videoids.append(search_result['id']['videoId'])
-
-    elif search_result['id']['kind'] == 'youtube#channel':
-      channels.append('%s (%s)' % (search_result['snippet']['title'],
-                                   search_result['id']['channelId']))
-      channelids.append(search_result['id']['channelId'])
-
-    elif search_result['id']['kind'] == 'youtube#playlist':
-      playlists.append('%s (%s)' % (search_result['snippet']['title'],
-                                    search_result['id']['playlistId']))
-      playlistids.append(search_result['id']['playlistId'])
-
-  #Results of YouTube search. If you wish to see the results, uncomment them
-  # print 'Videos:\n', '\n'.join(videos), '\n'
-  # print 'Channels:\n', '\n'.join(channels), '\n'
-  # print 'Playlists:\n', '\n'.join(playlists), '\n'
-
-  #Checks if your query is for a channel, playlist or a video and changes the URL accordingly
-  if 'channel'.lower() in  str(req).lower() and len(channels)!=0:
-      urlid=channelids[0]
-      YouTubeURL=("https://www.youtube.com/watch?v="+channelids[0])
-  elif 'playlist'.lower() in  str(req).lower() and len(playlists)!=0:
-      urlid=playlistids[0]
-      YouTubeURL=("https://www.youtube.com/watch?v="+playlistids[0])
-  else:
-      urlid=videoids[0]
-      YouTubeURL=("https://www.youtube.com/watch?v="+videoids[0])
-
- #If you want to see the URL, uncomment the following line
- #print(YouTubeURL)
-
- #Instead of sending it to Kodi, if you want to locally play in VLC, uncomment the following line and comment the second line
- #os.system("vlc "+YouTubeURL)
-  kodi.Player.open(item={"file":"plugin://plugin.video.youtube/?action=play_video&videoid=" + urlid})
-
-
 #Text to speech converter
 def say(words):
     tempfile = "temp.wav"
@@ -261,6 +197,73 @@ def feed(phrase):
             break
         else:
             continue
+
+            
+#Function to search YouTube and get videoid
+def youtube_search(query):
+  youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+    developerKey=DEVELOPER_KEY)
+
+  req=query
+  # Call the search.list method to retrieve results matching the specified
+  # query term.
+  search_response = youtube.search().list(
+    q=query,
+    part='id,snippet'
+  ).execute()
+
+  videos = []
+  channels = []
+  playlists = []
+  videoids = []
+  channelids = []
+  playlistids = []
+
+  # Add each result to the appropriate list, and then display the lists of
+  # matching videos, channels, and playlists.
+
+  for search_result in search_response.get('items', []):
+                  
+    if search_result['id']['kind'] == 'youtube#video':
+      videos.append('%s (%s)' % (search_result['snippet']['title'],
+                                 search_result['id']['videoId']))
+      videoids.append(search_result['id']['videoId'])
+
+    elif search_result['id']['kind'] == 'youtube#channel':
+      channels.append('%s (%s)' % (search_result['snippet']['title'],
+                                   search_result['id']['channelId']))
+      channelids.append(search_result['id']['channelId'])
+
+    elif search_result['id']['kind'] == 'youtube#playlist':
+      playlists.append('%s (%s)' % (search_result['snippet']['title'],
+                                    search_result['id']['playlistId']))
+      playlistids.append(search_result['id']['playlistId'])
+
+  #Results of YouTube search. If you wish to see the results, uncomment them
+  # print 'Videos:\n', '\n'.join(videos), '\n'
+  # print 'Channels:\n', '\n'.join(channels), '\n'
+  # print 'Playlists:\n', '\n'.join(playlists), '\n'
+
+  #Checks if your query is for a channel, playlist or a video and changes the URL accordingly
+  if 'channel'.lower() in  str(req).lower() and len(channels)!=0:
+      urlid=channelids[0]
+      YouTubeURL=("https://www.youtube.com/watch?v="+channelids[0])
+  elif 'playlist'.lower() in  str(req).lower() and len(playlists)!=0:
+      urlid=playlistids[0]
+      YouTubeURL=("https://www.youtube.com/watch?v="+playlistids[0])
+  else:
+      urlid=videoids[0]
+      YouTubeURL=("https://www.youtube.com/watch?v="+videoids[0])
+
+ #If you want to see the URL, uncomment the following line
+ #print(YouTubeURL)
+
+ #Instead of sending it to Kodi, if you want to locally play in VLC, uncomment the following two lines and comment the next two lines
+ #os.system("vlc "+YouTubeURL)
+ #say("Playing YouTube video")
+
+  kodi.Player.open(item={"file":"plugin://plugin.video.youtube/?action=play_video&videoid=" + urlid})
+  say("Playing YouTube video on Kodi")
 
 
 
