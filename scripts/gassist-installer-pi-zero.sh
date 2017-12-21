67031b66460e210cbc3f2ead2f2e5c6a363dc469
shivasiddharth <ushivasiddharth@gmail.com>
2017-12-21 13:36:54 +0000

    testing

--- scripts/gassist-installer-pi-zero.sh
+++ scripts/gassist-installer-pi-zero.sh
@@ -12,72 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-YES_ANSWER=1
-NO_ANSWER=2
-QUIT_ANSWER=3
-parse_user_input()
-{
-  if [ "$1" = "0" ] && [ "$2" = "0" ] && [ "$3" = "0" ]; then
-    return
-  fi
-  while [ true ]; do
-    Options="["
-    if [ "$1" = "1" ]; then
-      Options="${Options}y"
-      if [ "$2" = "1" ] || [ "$3" = "1" ]; then
-        Options="$Options/"
-      fi
-    fi
-    if [ "$2" = "1" ]; then
-      Options="${Options}n"
-      if [ "$3" = "1" ]; then
-        Options="$Options/"
-      fi
-    fi
-    if [ "$3" = "1" ]; then
-      Options="${Options}quit"
-    fi
-    Options="$Options]"
-    read -p "$Options >> " USER_RESPONSE
-    USER_RESPONSE=$(echo $USER_RESPONSE | awk '{print tolower($0)}')
-    if [ "$USER_RESPONSE" = "y" ] && [ "$1" = "1" ]; then
-      return $YES_ANSWER
-    else
-      if [ "$USER_RESPONSE" = "n" ] && [ "$2" = "1" ]; then
-        return $NO_ANSWER
-      else
-        if [ "$USER_RESPONSE" = "quit" ] && [ "$3" = "1" ]; then
-          printf "\nGoodbye.\n\n"
-          exit
-        fi
-      fi
-    fi
-    printf "Please enter a valid response.\n"
-  done
-}
-clear
-echo ""
-read -r -p "Enter the your full credential file name including .json extension: " credname
-echo ""
-read -r -p "Enter the your Google Cloud Console Project-Id: " projid
-echo ""
-read -r -p "Enter a nickname for your device: " nickname
-echo ""
-echo "Credentials file name is >> $credname"
-echo ""
-echo "Project id is >> $projid"
-echo ""
-echo "Nickname for your device is >> $nickname"
-echo ""
-echo ""
-echo "Is this information correct?"
-echo ""
-echo ""
-parse_user_input 1 1 0
-USER_RESPONSE=$?
-if [ "$USER_RESPONSE" = "$YES_ANSWER" ]; then
-  return
-fi
 
 set -o errexit
 
@@ -90,6 +24,15 @@ then
     echo "This script must run as $RUN_AS, trying to change user..."
     exec sudo -u $RUN_AS $0
 fi
+clear
+echo ""
+read -r -p "Enter the your full credential file name including .json extension: " credname
+echo ""
+read -r -p "Enter the your Google Cloud Console Project-Id: " projid
+echo ""
+read -r -p "Enter a nickname for your device: " nickname
+echo ""
+
 modelid=$projid-$(date +%F)
 
 cd /home/pi/
