4f601bdc18f2d2ba3e8e330c73dc056433840c9f
shivasiddharth <ushivasiddharth@gmail.com>
2017-12-22 19:15:14 +0000

    test

--- scripts/gassist-installer-pi-zero.sh
+++ scripts/gassist-installer-pi-zero.sh
@@ -47,6 +47,7 @@ sudo apt-get update -y
 sudo apt-get install portaudio19-dev libffi-dev libssl-dev -y
 sudo apt-get install libttspico0 libttspico-utils libttspico-data -y
 #--------------------------------------------------------------------------------
+sudo apt-get install python3-dev python3-venv -y
 python3 -m venv env
 env/bin/python -m pip install --upgrade pip setuptools
 source env/bin/activate
