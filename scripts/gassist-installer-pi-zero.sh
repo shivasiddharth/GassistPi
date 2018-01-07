fad03a6151b43c26d0fec3219e761f3c093106c2
shivasiddharth <ushivasiddharth@gmail.com>
2018-01-07 22:00:14 +0000

    chk

--- scripts/gassist-installer-pi-zero.sh
+++ scripts/gassist-installer-pi-zero.sh
@@ -37,8 +37,9 @@ modelid=$projid-$(date +%Y%m%d%H%M%S )
 echo "Your Model-Id used for the project is: $modelid" >> /home/pi/modelid.txt
 cd /home/pi/
 #--------------GassistPi Deps----------------------------------------------------
-sudo apt-get install libxml2-dev libxslt-dev python-dev
+sudo apt-get install libxml2-dev libxslt-dev python-dev -y
 sudo apt-get install mpv -y
+mkdir /home/pi/.config/mpv/
 mkdir /home/pi/.config/mpv/scripts/
 mv /home/pi/GassistPi/src/end.lua /home/pi/.config/mpv/scripts/end.lua
 sudo apt-get install mplayer -y
