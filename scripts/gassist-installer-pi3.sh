baac4e1b887fdb816985eaa691af434e6656fb13
shivasiddharth <ushivasiddharth@gmail.com>
2018-01-24 15:44:20 +0000

    Update gassist-installer-pi3.sh

--- scripts/gassist-installer-pi3.sh
+++ scripts/gassist-installer-pi3.sh
@@ -55,6 +55,7 @@ sudo apt-get install libttspico0 libttspico-utils libttspico-data -y
 python3 -m venv env
 env/bin/python -m pip install --upgrade pip setuptools
 source env/bin/activate
+pip install -r /home/pi/GassistPi/requirements.txt
 pip install wheel
 pip install RPi.GPIO
 #--------------GassistPi Deps----------------------------------------------------
