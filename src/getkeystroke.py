8bee5679e0edb6d5fab02409b38a9ab78f211f6e
MOHAMMAD RASIM <mohammad.rasim96@gmail.com>
2018-03-01 03:01:38 +0300

    add missing files for keyboard capture

---
+++ src/getkeystroke.py
@@ -0,0 +1,28 @@
+
+import pyxhook
+import time
+
+# This function is called every time a key is presssed
+def kbevent(event):
+    global running
+    # print key info
+
+    print('Ascii value: {}'.format(event.Ascii))
+
+
+# Create hookmanager
+hookman = pyxhook.HookManager()
+# Define our callback to fire when a key is pressed down
+hookman.KeyDown = kbevent
+# Hook the keyboard
+hookman.HookKeyboard()
+# Start our listener
+hookman.start()
+
+# Create a loop to keep the application running
+running = True
+while running:
+    time.sleep(0.1)
+
+# Close the listener when we are done
+hookman.cancel()
