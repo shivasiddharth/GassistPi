d2a1ca6c482719547eb8da516dbc331be3f66338
MOHAMMAD RASIM <mohammad.rasim96@gmail.com>
2018-03-06 00:22:34 +0300

    updates (#248)
    * Allow settings to be saved
    No need to edit the services files manually anymore
    
    * 1-combine installers in one file
    2-change credentials file name to gassistant-credentials.info
    3-cleanups
    
    * check hardware architecture
    
    * make installer more portable
    
    * fix errors in the installer and serivce installer
    
    * Update README.md
    
    * make the project more portable (#206)
    
    * Allow settings to be saved
    No need to edit the services files manually anymore
    
    * 1-combine installers in one file
    2-change credentials file name to gassistant-credentials.info
    3-cleanups
    
    * check hardware architecture
    
    * make installer more portable
    
    * fix errors in the installer and serivce installer
    
    * Update gassist-installer.sh
    
    * play sounds in a more pythonic way(using pygame)
    port the project to platforms other than Raspberrypi
    add archlinux support
    more portability changes
    
    * parse gassistant-credentials.info file inside python script
    
    * fix last merge errors and indentations
    
    * trigger pushbutton.py using the keyboard in platforms without gpios
    
    * add missing files for keyboard capture
    
    * control tasmota with multiple relays
    
    * read args from file
    
    * control tasmota with multiple relays
    
    * update getkeystoke to be clearer
    
    * portability changes and refactoring
    remove mpg123 and use pygame
    refactor tasmota devices control
    
    * Update README.md
    
    * update README.md
    
    * make pygame plays nicely with devices running pulseaudio
    
    * delete old installers
    
    * fix previous merging error

---
+++ scripts/gassist-installer.sh
@@ -0,0 +1,150 @@
+#!/bin/bash
+# Copyright 2017 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+set -o errexit
+
+scripts_dir="$(dirname "${BASH_SOURCE[0]}")"
+GIT_DIR="$(realpath $(dirname ${BASH_SOURCE[0]})/..)"
+
+
+#check device architecture
+if [[ $(uname -m|grep "armv7") ]]; then
+	devmodel="armv7"
+else
+	devmodel="armv6"
+fi
+
+# make sure we're running as the owner of the checkout directory
+RUN_AS="$(ls -ld "$scripts_dir" | awk 'NR==1 {print $3}')"
+if [ "$USER" != "$RUN_AS" ]
+then
+    echo "This script must run as $RUN_AS, trying to change user..."
+    exec sudo -u $RUN_AS $0
+fi
+INFO_FILE="/home/${USER}/gassistant-credentials.info"
+
+clear
+if [ -f $INFO_FILE ]
+then
+    . $INFO_FILE
+fi
+
+if [[ $credname != "" ]]
+then
+
+   credmsg="If your credentials file name is $credname then press enter. Else, enter your full credential file name including .json extension: "
+else
+    credmsg="Enter your full credential file name including .json extension: "
+fi
+
+if [[ $project_id != "" ]]
+then
+    projidmsg="If your Project-Id is $projid then press enter. Else, enter your Google Cloud Console Project-Id: "
+
+else
+    projidmsg="Enter your Google Cloud Console Project-Id: "
+fi
+
+if [[ $prodname != "" ]]
+then
+    prodmsg="If your Product name is $prodname then press enter. Else, enter a product name for your device (product name should not have space in between): "
+else
+    prodmsg="Enter a product name for your device (product name should not have space in between): "
+fi
+
+
+
+echo ""
+echo -e $credmsg
+read -r tmp
+
+if [[ $tmp != "" ]]
+then 
+	credname=$tmp
+fi
+echo ""
+echo -e $projidmsg
+read -r tmp
+if [[ $tmp != "" ]]
+then 
+	project_id=$tmp
+fi
+echo ""
+echo -e $prodmsg
+read -r tmp
+if [[ $tmp != "" ]]
+then 
+	prodname=$tmp
+fi
+echo ""
+
+
+device_model_id=$project_id-$(date +%Y%m%d%H%M%S )
+echo "" > $INFO_FILE
+echo "credname='$credname'" >> $INFO_FILE
+echo "project_id='$project_id'" >> $INFO_FILE
+echo "prodname='$prodname'" >> $INFO_FILE
+echo "device_model_id='$device_model_id'" >> $INFO_FILE
+
+
+cd /home/${USER}
+
+
+
+if hash apt-get >/dev/null 2>&1;then
+  sudo apt-get update -y
+  sed 's/#.*//' ${GIT_DIR}/Requirements/GassistPi-system-requirements.txt | xargs sudo apt-get install -y
+elif hash pacman >/dev/null 2>&1;then
+#  sudo pacman -Sy
+  sed 's/#.*//' ${GIT_DIR}/Requirements/GassistPi-system-requirements-archlinux.txt | xargs sudo pacman -S --noconfirm --needed
+fi
+
+if [ ! -d /home/${USER}/.config/mpv/scripts/ ]; then
+  mkdir -p /home/${USER}/.config/mpv/scripts/
+fi
+if [ -f ${GIT_DIR}/src/end.lua ]; then
+  cp ${GIT_DIR}/src/end.lua /home/${USER}/.config/mpv/scripts/end.lua
+fi
+if [ -f ${GIT_DIR}/src/mpv.conf ]; then
+  cp ${GIT_DIR}/src/mpv.conf /home/${USER}/.config/mpv/mpv.conf
+fi
+
+
+python3 -m venv env
+env/bin/python -m pip install --upgrade pip setuptools wheel
+source env/bin/activate
+
+pip install -r ${GIT_DIR}/Requirements/GassistPi-pip-requirements.txt
+
+if [[ $devmodel = "armv7" ]];then
+	pip install google-assistant-library==0.1.0
+fi
+
+pip install google-assistant-grpc==0.1.0
+pip install google-assistant-sdk==0.4.2
+pip install google-assistant-sdk[samples]==0.4.2
+pip install google-auth==1.3.0	google-auth-httplib2==0.0.3 google-auth-oauthlib==0.2.0
+google-oauthlib-tool --client-secrets /home/${USER}/$credname --scope https://www.googleapis.com/auth/assistant-sdk-prototype --save --headless
+googlesamples-assistant-devicetool register-model --manufacturer "Pi Foundation" \
+          --product-name $prodname --type LIGHT --model $device_model_id
+          
+echo "Testing the installed google assistant. Make a note of the generated Device-Id"
+
+if [[ $devmodel = "armv7" ]];then
+	googlesamples-assistant-hotword --project_id $project_id --device_model_id $device_model_id
+else
+	googlesamples-assistant-pushtotalk --project-id $project_id --device-model-id $device_model_id
+fi
+
+
--- src/main.py
+++ src/main.py
@@ -15,7 +15,12 @@
 # limitations under the License.
 
 from kodijson import Kodi, PLAYER_VIDEO
-import RPi.GPIO as GPIO
+# test if we are running in raspberrypi
+try:
+    import RPi.GPIO as GPIO
+except Exception as e:
+    if str(e) == 'This module can only be run on a Raspberry Pi!':
+        GPIO = None
 import argparse
 import os.path
 import os
@@ -24,6 +29,7 @@ import subprocess
 import re
 import psutil
 import logging
+import imp
 import google.auth.transport.requests
 import google.oauth2.credentials
 from google.assistant.library import Assistant
@@ -49,13 +55,18 @@ from actions import chromecast_control
 from actions import kickstarter_tracker
 from actions import getrecipe
 from actions import hue_control
+from actions import play_audio_file
+from actions import tasmota_control
+from actions import tasmota_devicelist
 
+ROOT_PATH = os.path.realpath(os.path.join(__file__, '..', '..'))
+resources = {'fb': '{}/sample-audio-files/Fb.wav'.format(ROOT_PATH), 'startup': '{}/sample-audio-files/Startup.wav'.format(ROOT_PATH)}
 
 logging.basicConfig(filename='/tmp/GassistPi.log', level=logging.DEBUG,
                     format='%(asctime)s %(levelname)s %(name)s %(message)s')
 logger=logging.getLogger(__name__)
 
-
+INFO_FILE = os.path.expanduser('~/gassistant-credentials.info')
 
 #Login with default kodi/kodi credentials
 #kodi = Kodi("http://localhost:8080/jsonrpc")
@@ -64,25 +75,22 @@ logger=logging.getLogger(__name__)
 # Kodi("http://IP-ADDRESS-OF-KODI:8080/jsonrpc", "username", "password")
 kodi = Kodi("http://192.168.1.15:8080/jsonrpc", "kodi", "kodi")
 
+if GPIO != None:
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+    # Indicator Pins
+    GPIO.setup(25, GPIO.OUT)
+    GPIO.setup(5, GPIO.OUT)
+    GPIO.setup(6, GPIO.OUT)
+    GPIO.output(5, GPIO.LOW)
+    GPIO.output(6, GPIO.LOW)
+    led = GPIO.PWM(25, 1)
+    led.start(0)
 
-#Indicator Pins
-GPIO.setup(25, GPIO.OUT)
-GPIO.setup(5, GPIO.OUT)
-GPIO.setup(6, GPIO.OUT)
-GPIO.output(5, GPIO.LOW)
-GPIO.output(6, GPIO.LOW)
-led=GPIO.PWM(25,1)
-led.start(0)
+mpvactive = False
 
-mpvactive=False
 
-#Sonoff-Tasmota Declarations
-#Make sure that the device name assigned here does not overlap any of your smart device names in the google home app
-tasmota_devicelist=['Desk Light','Table Light']
-tasmota_deviceip=['192.168.1.35','192.168.1.36']
 
 #Magic Mirror Remote Control Declarations
 mmmip='ENTER_YOUR_MAGIC_MIRROR_IP'
@@ -100,17 +108,6 @@ def ismpvplaying():
     return mpvactive
 
 
-#Function to control Sonoff Tasmota Devices
-def tasmota_control(phrase,devname,devip):
-    try:
-        if 'on' in phrase:
-            rq=requests.head("http://"+devip+"/cm?cmnd=Power%20on")
-            say("Tunring on "+devname)
-        elif 'off' in phrase:
-            rq=requests.head("http://"+devip+"/cm?cmnd=Power%20off")
-            say("Tunring off "+devname)
-    except requests.exceptions.ConnectionError:
-        say("Device not online")
 
 def process_device_actions(event, device_id):
     if 'inputs' in event.args:
@@ -135,24 +132,25 @@ def process_event(event, device_id):
         event(event.Event): The current event to process.
     """
     if event.type == EventType.ON_CONVERSATION_TURN_STARTED:
-        subprocess.Popen(["aplay", "/home/pi/GassistPi/sample-audio-files/Fb.wav"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        play_audio_file(resources['fb'])
         #Uncomment the following after starting the Kodi
         #status=mutevolstatus()
         #vollevel=status[1]
-        #with open('/home/pi/.volume.json', 'w') as f:
+        #with open(os.path.expanduser('~/.volume.json'), 'w') as f:
                #json.dump(vollevel, f)
         #kodi.Application.SetVolume({"volume": 0})
-        GPIO.output(5,GPIO.HIGH)
-        led.ChangeDutyCycle(100)
+        if GPIO != None:
+            GPIO.output(5, GPIO.HIGH)
+            led.ChangeDutyCycle(100)
         print()
         if ismpvplaying():
-            if os.path.isfile("/home/pi/.mediavolume.json"):
+            if os.path.isfile(os.path.expanduser("~/.mediavolume.json")):
                 mpvsetvol=os.system("echo '"+json.dumps({ "command": ["set_property", "volume","10"]})+"' | socat - /tmp/mpvsocket")
             else:
                 mpvgetvol=subprocess.Popen([("echo '"+json.dumps({ "command": ["get_property", "volume"]})+"' | socat - /tmp/mpvsocket")],shell=True, stdout=subprocess.PIPE)
                 output=mpvgetvol.communicate()[0]
                 for currntvol in re.findall(r"[-+]?\d*\.\d+|\d+", str(output)):
-                    with open('/home/pi/.mediavolume.json', 'w') as vol:
+                    with open(os.path.expanduser('~/.mediavolume.json'), 'w') as vol:
                         json.dump(currntvol, vol)
                 mpvsetvol=os.system("echo '"+json.dumps({ "command": ["set_property", "volume","10"]})+"' | socat - /tmp/mpvsocket")
 
@@ -161,50 +159,54 @@ def process_event(event, device_id):
         print(event)
 
     if event.type == EventType.ON_CONVERSATION_TURN_TIMEOUT:
-      GPIO.output(5,GPIO.LOW)
-      GPIO.output(6,GPIO.LOW)
-      led.ChangeDutyCycle(0)
-        #Uncomment the following after starting the Kodi
-        #with open('/home/pi/.volume.json', 'r') as f:
-               #vollevel = json.load(f)
-               #kodi.Application.SetVolume({"volume": vollevel})
-      if ismpvplaying():
-          if os.path.isfile("/home/pi/.mediavolume.json"):
-              with open('/home/pi/.mediavolume.json', 'r') as vol:
+        if GPIO != None:
+            GPIO.output(5, GPIO.LOW)
+            GPIO.output(6, GPIO.LOW)
+            led.ChangeDutyCycle(0)
+        # Uncomment the following after starting the Kodi
+        # with open('/home/pi/.volume.json', 'r') as f:
+               # vollevel = json.load(f)
+               # kodi.Application.SetVolume({"volume": vollevel})
+        if ismpvplaying():
+            if os.path.isfile(os.path.expanduser("~/.mediavolume.json")):
+              with open(os.path.expanduser('~/.mediavolume.json'), 'r') as vol:
                   oldvollevel = json.load(vol)
               print(oldvollevel)
               mpvsetvol=os.system("echo '"+json.dumps({ "command": ["set_property", "volume",str(oldvollevel)]})+"' | socat - /tmp/mpvsocket")
 
-
     if (event.type == EventType.ON_RESPONDING_STARTED and event.args and not event.args['is_error_response']):
-       GPIO.output(5,GPIO.LOW)
-       GPIO.output(6,GPIO.HIGH)
-       led.ChangeDutyCycle(50)
+        if GPIO != None:
+            GPIO.output(5, GPIO.LOW)
+            GPIO.output(6, GPIO.HIGH)
+            led.ChangeDutyCycle(50)
 
     if event.type == EventType.ON_RESPONDING_FINISHED:
-       GPIO.output(6,GPIO.LOW)
-       GPIO.output(5,GPIO.HIGH)
-       led.ChangeDutyCycle(100)
+        if GPIO != None:
+            GPIO.output(6, GPIO.LOW)
+            GPIO.output(5, GPIO.HIGH)
+            led.ChangeDutyCycle(100)
 
     if event.type == EventType.ON_RECOGNIZING_SPEECH_FINISHED:
-       GPIO.output (5, GPIO.LOW)
-       GPIO.output (6, GPIO.LOW)
-       led.ChangeDutyCycle (0)
+        if GPIO != None:
+            GPIO.output(5, GPIO.LOW)
+            GPIO.output(6, GPIO.LOW)
+            led.ChangeDutyCycle(0)
 
     print(event)
 
     if (event.type == EventType.ON_CONVERSATION_TURN_FINISHED and
             event.args and not event.args['with_follow_on_turn']):
-        GPIO.output(5,GPIO.LOW)
-        GPIO.output(6,GPIO.LOW)
-        led.ChangeDutyCycle(0)
-        #Uncomment the following after starting the Kodi
-        #with open('/home/pi/.volume.json', 'r') as f:
-               #vollevel = json.load(f)
-               #kodi.Application.SetVolume({"volume": vollevel})
+        if GPIO != None:
+            GPIO.output(5, GPIO.LOW)
+            GPIO.output(6,GPIO.LOW)
+            led.ChangeDutyCycle(0)
+        # Uncomment the following after starting the Kodi
+        # with open(os.path.expanduser('~/.volume.json'), 'r') as f:
+               # vollevel = json.load(f)
+               # kodi.Application.SetVolume({"volume": vollevel})
         if ismpvplaying():
-            if os.path.isfile("/home/pi/.mediavolume.json"):
-                with open('/home/pi/.mediavolume.json', 'r') as vol:
+            if os.path.isfile(os.path.expanduser("~/.mediavolume.json")):
+                with open(os.path.expanduser('~/.mediavolume.json'), 'r') as vol:
                     oldvollevel = json.load(vol)
                 print(oldvollevel)
                 mpvsetvol=os.system("echo '"+json.dumps({ "command": ["set_property", "volume",str(oldvollevel)]})+"' | socat - /tmp/mpvsocket")
@@ -246,38 +248,16 @@ def register_device(project_id, credentials, device_model_id, device_id):
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument('--credentials', type=existing_file,
-                        metavar='OAUTH2_CREDENTIALS_FILE',
-                        default=os.path.join(
-                            os.path.expanduser('~/.config'),
-                            'google-oauthlib-tool',
-                            'credentials.json'
-                        ),
-                        help='Path to store and read OAuth2 credentials')
-    parser.add_argument('--device_model_id', type=str,
-                        metavar='DEVICE_MODEL_ID', required=True,
-                        help='The device model ID registered with Google.')
-    parser.add_argument(
-        '--project_id',
-        type=str,
-        metavar='PROJECT_ID',
-        required=False,
-        help='The project ID used to register device instances.')
-    parser.add_argument(
-        '-v',
-        '--version',
-        action='version',
-        version='%(prog)s ' +
-        Assistant.__version_str__())
+    args = imp.load_source('args',INFO_FILE)
+    if not hasattr(args,'credentials'):
+        args.credentials = os.path.join(os.path.expanduser('~/.config'),'google-oauthlib-tool','credentials.json')
 
-    args = parser.parse_args()
+
     with open(args.credentials, 'r') as f:
         credentials = google.oauth2.credentials.Credentials(token=None,
                                                             **json.load(f))
     with Assistant(credentials, args.device_model_id) as assistant:
-        subprocess.Popen(["aplay", "/home/pi/GassistPi/sample-audio-files/Startup.wav"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        play_audio_file(resources['startup'])
         events = assistant.start()
         print('device_model_id:', args.device_model_id + '\n' +
               'device_id:', assistant.device_id + '\n')
@@ -287,7 +267,7 @@ def main():
         for event in events:
             process_event(event, assistant.device_id)
             usrcmd=event.args
-            with open('/home/pi/GassistPi/src/diyHue/config.json', 'r') as config:
+            with open('{}/src/diyHue/config.json'.format(ROOT_PATH), 'r') as config:
                  hueconfig = json.load(config)
             for i in range(1,len(hueconfig['lights'])+1):
                 try:
@@ -298,10 +278,10 @@ def main():
                 except Keyerror:
                     say('Unable to help, please check your config file')
 
-            for num, name in enumerate(tasmota_devicelist):
-                if name.lower() in str(usrcmd).lower():
+            for item in tasmota_devicelist:
+                if item['friendly-name'].lower()  in str(usrcmd).lower():
                     assistant.stop_conversation()
-                    tasmota_control(str(usrcmd).lower(), name.lower(),tasmota_deviceip[num])
+                    tasmota_control(usrcmd['text'], item)
                     break
             if 'magic mirror'.lower() in str(usrcmd).lower():
                 assistant.stop_conversation()
@@ -339,24 +319,22 @@ def main():
             if 'kickstarter'.lower() in str(usrcmd).lower():
                 assistant.stop_conversation()
                 kickstarter_tracker(str(usrcmd).lower())
-            if 'trigger'.lower() in str(usrcmd).lower():
+            if 'trigger'.lower() in str(usrcmd).lower() and GPIO != None:
                 assistant.stop_conversation()
                 Action(str(usrcmd).lower())
             if 'stream'.lower() in str(usrcmd).lower():
                 assistant.stop_conversation()
                 os.system('pkill mpv')
-                if os.path.isfile("/home/pi/GassistPi/src/trackchange.py"):
-                    os.system('rm /home/pi/GassistPi/src/trackchange.py')
+                if os.path.isfile("{}/src/trackchange.py".format(ROOT_PATH)):
+                    os.system('rm {}/src/trackchange.py'.format(ROOT_PATH))
                     if 'autoplay'.lower() in str(usrcmd).lower():
-                        os.system('echo "from actions import youtubeplayer\n\n" >> /home/pi/GassistPi/src/trackchange.py')
-                        os.system('echo "youtubeplayer()\n" >> /home/pi/GassistPi/src/trackchange.py')
                         YouTube_Autoplay(str(usrcmd).lower())
                     else:
                         YouTube_No_Autoplay(str(usrcmd).lower())
                 else:
                     if 'autoplay'.lower() in str(usrcmd).lower():
-                        os.system('echo "from actions import youtubeplayer\n\n" >> /home/pi/GassistPi/src/trackchange.py')
-                        os.system('echo "youtubeplayer()\n" >> /home/pi/GassistPi/src/trackchange.py')
+                        os.system('echo "from actions import youtubeplayer\n\n" >> {}/src/trackchange.py'.format(ROOT_PATH))
+                        os.system('echo "youtubeplayer()\n" >> {}/src/trackchange.py'.format(ROOT_PATH))
                         YouTube_Autoplay(str(usrcmd).lower())
                     else:
                         YouTube_No_Autoplay(str(usrcmd).lower())
@@ -398,22 +376,22 @@ def main():
                     if 'set'.lower() in str(usrcmd).lower() or 'change'.lower() in str(usrcmd).lower():
                         if 'hundred'.lower() in str(usrcmd).lower() or 'maximum' in str(usrcmd).lower():
                             settingvollevel=100
-                            with open('/home/pi/.mediavolume.json', 'w') as vol:
+                            with open(os.path.expanduser('~/.mediavolume.json'), 'w') as vol:
                                 json.dump(settingvollevel, vol)
                             mpvsetvol=os.system("echo '"+json.dumps({ "command": ["set_property", "volume",str(settingvollevel)]})+"' | socat - /tmp/mpvsocket")
                         elif 'zero'.lower() in str(usrcmd).lower() or 'minimum' in str(usrcmd).lower():
                             settingvollevel=0
-                            with open('/home/pi/.mediavolume.json', 'w') as vol:
+                            with open(os.path.expanduser('~/.mediavolume.json'), 'w') as vol:
                                 json.dump(settingvollevel, vol)
                             mpvsetvol=os.system("echo '"+json.dumps({ "command": ["set_property", "volume",str(settingvollevel)]})+"' | socat - /tmp/mpvsocket")
                         else:
                             for settingvollevel in re.findall(r"[-+]?\d*\.\d+|\d+", str(usrcmd)):
-                                with open('/home/pi/.mediavolume.json', 'w') as vol:
+                                with open(os.path.expanduser('~/.mediavolume.json'), 'w') as vol:
                                     json.dump(settingvollevel, vol)
                             mpvsetvol=os.system("echo '"+json.dumps({ "command": ["set_property", "volume",str(settingvollevel)]})+"' | socat - /tmp/mpvsocket")
                     elif 'increase'.lower() in str(usrcmd).lower() or 'decrease'.lower() in str(usrcmd).lower() or 'reduce'.lower() in str(usrcmd).lower():
-                        if os.path.isfile("/home/pi/.mediavolume.json"):
-                            with open('/home/pi/.mediavolume.json', 'r') as vol:
+                        if os.path.isfile(os.path.expanduser("~/.mediavolume.json")):
+                            with open(os.path.expanduser('~/.mediavolume.json'), 'r') as vol:
                                 oldvollevel = json.load(vol)
                                 for oldvollevel in re.findall(r'\b\d+\b', str(oldvollevel)):
                                     oldvollevel=int(oldvollevel)
@@ -437,7 +415,7 @@ def main():
                                 settingvollevel==0
                             else:
                                 settingvollevel=newvollevel
-                            with open('/home/pi/.mediavolume.json', 'w') as vol:
+                            with open(os.path.expanduser('~/.mediavolume.json'), 'w') as vol:
                                 json.dump(settingvollevel, vol)
                             mpvsetvol=os.system("echo '"+json.dumps({ "command": ["set_property", "volume",str(settingvollevel)]})+"' | socat - /tmp/mpvsocket")
                         if 'decrease'.lower() in str(usrcmd).lower() or 'reduce'.lower() in str(usrcmd).lower():
@@ -454,7 +432,7 @@ def main():
                                 settingvollevel==0
                             else:
                                 settingvollevel=newvollevel
-                            with open('/home/pi/.mediavolume.json', 'w') as vol:
+                            with open(os.path.expanduser('~/.mediavolume.json'), 'w') as vol:
                                 json.dump(settingvollevel, vol)
                             mpvsetvol=os.system("echo '"+json.dumps({ "command": ["set_property", "volume",str(settingvollevel)]})+"' | socat - /tmp/mpvsocket")
                     else:
@@ -468,8 +446,8 @@ def main():
             if 'google music'.lower() in str(usrcmd).lower():
                 assistant.stop_conversation()
                 os.system('pkill mpv')
-                if os.path.isfile("/home/pi/GassistPi/src/trackchange.py"):
-                    os.system('rm /home/pi/GassistPi/src/trackchange.py')
+                if os.path.isfile("{}/src/trackchange.py".format(ROOT_PATH)):
+                    os.system('rm {}/src/trackchange.py'.format(ROOT_PATH))
                     gmusicselect(str(usrcmd).lower())
                 else:
                     gmusicselect(str(usrcmd).lower())
