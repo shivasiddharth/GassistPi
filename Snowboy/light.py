0fe4c9891bbee0e0abcf48589cbac07ec34bef89
aandroide <djenzo147@gmail.com>
2018-06-26 21:05:26 +0200

    Update light.py

--- Snowboy/light.py
+++ Snowboy/light.py
@@ -34,7 +34,7 @@ class Light(object):
         self.set_off()
 
 if __name__ == "__main__":
-    light = Light(23)
+    light = Light(22)
     while True:
         light.blink()
         time.sleep(0.5)
