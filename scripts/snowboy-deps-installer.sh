b7e72c87b126f576010876c2945a93bb6fbd862b
aandroide <djenzo147@gmail.com>
2018-06-26 20:08:50 +0200

    snowboy-deps-installer.sh

---
+++ scripts/snowboy-deps-installer.sh
@@ -0,0 +1,19 @@
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+
+scripts_dir="$(dirname "${BASH_SOURCE[0]}")"
+
+# make sure we're running as the owner of the checkout directory
+RUN_AS="$(ls -ld "$scripts_dir" | awk 'NR==1 {print $3}')"
+if [ "$USER" != "$RUN_AS" ]
+then
+    echo "This script must run as $RUN_AS, trying to change user..."
+    exec sudo -u $RUN_AS $0
+fi
+sudo apt-get install libjack-jackd2-dev -y
+sudo apt-get install portaudio19-dev libffi-dev libssl-dev -y
+sudo apt-get install swig3.0 python-pyaudio python3-pyaudio sox -y
+sudo pip install pyaudio
+sudo apt-get install libatlas-base-dev -y
