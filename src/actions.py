9a0f7e343e3704aa311e90e4992136423702f3f8
shivasiddharth <ushivasiddharth@gmail.com>
2018-10-11 10:36:26 +0100

    Added spanish language support for custom actions

--- src/actions.py
+++ src/actions.py
@@ -54,6 +54,8 @@ elif configuration['Language']['Choice']=='fr':
     keywordfile= '{}/src/keywords_fr.yaml'.format(ROOT_PATH)
 elif configuration['Language']['Choice']=='de':
     keywordfile= '{}/src/keywords_de.yaml'.format(ROOT_PATH)
+elif configuration['Language']['Choice']=='es':
+    keywordfile= '{}/src/keywords_es.yaml'.format(ROOT_PATH)
 else:
     keywordfile= '{}/src/keywords_en.yaml'.format(ROOT_PATH)
 with open(keywordfile,'r') as conf:
