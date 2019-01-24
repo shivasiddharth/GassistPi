70dda6bb4bbb9a8e25b761d58024bcdbf208b320
aandroide <djenzo147@gmail.com>
2019-01-24 22:20:26 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -611,7 +611,7 @@ class Myassistant():
             ingrequest=ingrequest.replace(" ","%20",1)
             getrecipe(ingrequest)
         if configuration['Send_Message_Control']['Send_Control']=='Enabled':
-            if (custom_action_keyword['Keywords']['Send_Message_control'][0]).lower() in str(usrcmd).lower():
+            if (custom_action_keyword['Keywords']['Send_Message'][0]).lower() in str(usrcmd).lower():
                 self.assistant.stop_conversation()
                 message(str(usrcmd).lower())
         if (custom_action_keyword['Keywords']['Kickstarter_tracking'][0]).lower() in str(usrcmd).lower():
