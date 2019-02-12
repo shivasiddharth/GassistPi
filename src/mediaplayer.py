f71ed55567bafb78ff0fc2d27bd6a08084869215
shivasiddharth <ushivasiddharth@gmail.com>
2019-02-12 13:51:51 +0000

    Update mediaplayer.py
    Fixes #826

--- src/mediaplayer.py
+++ src/mediaplayer.py
@@ -18,14 +18,15 @@ with open('{}/src/config.yaml'.format(ROOT_PATH),'r') as conf:
 
 api = Mobileclient()
 logged_in=api.oauth_login(Mobileclient.FROM_MAC_ADDRESS)
+if logged_in:
+    for device in api.get_registered_devices():
+        if device['type']=='ANDROID':
+            deviceid=device['id'][2:]
+            print(deviceid)
+            break
+    api.logout()
+    logged_in=api.oauth_login(deviceid)
 
-for device in api.get_registered_devices():
-    if device['type']=='ANDROID':
-        deviceid=device['id'][2:]
-        print(deviceid)
-        break
-api.logout()
-logged_in=api.oauth_login(deviceid)
 
 
 class vlcplayer():
