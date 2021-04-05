9ac2372154d2d130fbacbdd195dd6e3d32144566
shivasiddharth <ushivasiddharth@gmail.com>
2021-04-05 20:51:34 +0530

    Update indicator.py

--- src/indicator.py
+++ src/indicator.py
@@ -11,6 +11,8 @@ import apa102
 import time
 import threading
 import numpy
+import usb.core
+import usb.util
 from gpiozero import LED
 try:
     import queue as Queue
@@ -34,6 +36,8 @@ if os.path.isfile("{}/audiosetup".format(USER_PATH)):
             audiosetup='R4M'
         elif (detected_audio_setup=='Respeaker-2-Mic'):
             audiosetup='R2M'
+        elif (detected_audio_setup=='Respeaker-USB-Mic'):
+            audiosetup='RUM'
         else:
             audiosetup='GEN'
 else:
@@ -305,10 +309,96 @@ class Pixels2mic:
     def mute(self):
         self.write([1,0,0] * self.PIXELS_N)
 
+class PixelRing:
+    TIMEOUT = 8000
+
+    def __init__(self, dev):
+        self.dev = dev
+
+    def trace(self):
+        self.write(0)
+
+    def mono(self, color):
+        self.write(1, [(color >> 16) & 0xFF, (color >> 8) & 0xFF, color & 0xFF, 0])
+
+    def set_color(self, rgb=None, r=0, g=0, b=0):
+        if rgb:
+            self.mono(rgb)
+        else:
+            self.write(1, [r, g, b, 0])
+
+    def off(self):
+        self.mono(0)
+
+    def listen(self, direction=None):
+        self.write(2)
+
+    wakeup = listen
+
+    def speak(self):
+        self.write(3)
+
+    def think(self):
+        self.write(4)
+
+    wait = think
+
+    def spin(self):
+        self.write(5)
+
+    def show(self, data):
+        self.write(6, data)
+
+    customize = show
+
+    def set_brightness(self, brightness):
+        self.write(0x20, [brightness])
+
+    def set_color_palette(self, a, b):
+        self.write(0x21, [(a >> 16) & 0xFF, (a >> 8) & 0xFF, a & 0xFF, 0, (b >> 16) & 0xFF, (b >> 8) & 0xFF, b & 0xFF, 0])
+
+    def set_vad_led(self, state):
+        self.write(0x22, [state])
+
+    def set_volume(self, volume):
+        self.write(0x23, [volume])
+
+    def change_pattern(self, pattern=None):
+        print('Not support to change pattern')
+
+    def write(self, cmd, data=[0]):
+        self.dev.ctrl_transfer(
+            usb.util.CTRL_OUT | usb.util.CTRL_TYPE_VENDOR | usb.util.CTRL_RECIPIENT_DEVICE,
+            0, cmd, 0x1C, data, self.TIMEOUT)
+
+    def close(self):
+        """
+        close the interface
+        """
+        usb.util.dispose_resources(self.dev)
+
+def find(vid=0x2886, pid=0x0018):
+    dev = usb.core.find(idVendor=vid, idProduct=pid)
+    if not dev:
+        return
+
+    # configuration = dev.get_active_configuration()
+
+    # interface_number = None
+    # for interface in configuration:
+    #     interface_number = interface.bInterfaceNumber
+
+    #     if dev.is_kernel_driver_active(interface_number):
+    #         dev.detach_kernel_driver(interface_number)
+
+    return PixelRing(dev)
+
 if audiosetup=='R2M':
     pixels=Pixels2mic()
 elif audiosetup=='R4M':
     pixels=Pixels4mic()
+elif audiosetup=='RUM':
+    pixel_ring = find()
 
 def assistantindicator(activity):
     activity=activity.lower()
@@ -320,6 +410,8 @@ def assistantindicator(activity):
             pixels.listen()
         elif (audiosetup=='AIY'):
             led.ChangeDutyCycle(75)
+        elif (audiosetup=='RUM'):
+            pixel_ring.listen()
     elif activity=='speaking':
         if (audiosetup=='GEN'):
             GPIO.output(speakingindicator,GPIO.HIGH)
@@ -328,6 +420,8 @@ def assistantindicator(activity):
             pixels.speak()
         elif (audiosetup=='AIY'):
             led.ChangeDutyCycle(50)
+        elif (audiosetup=='RUM'):
+            pixel_ring.speak()
     elif (activity=='off' or activity=='unmute'):
         if (audiosetup=='GEN'):
             GPIO.output(speakingindicator,GPIO.LOW)
@@ -336,6 +430,8 @@ def assistantindicator(activity):
             pixels.off()
         elif (audiosetup=='AIY'):
             led.ChangeDutyCycle(0)
+        elif (audiosetup=='RUM'):
+            pixel_ring.off()
     elif (activity=='on' or activity=='mute'):
         if (audiosetup=='GEN'):
             GPIO.output(speakingindicator,GPIO.HIGH)
@@ -344,3 +440,5 @@ def assistantindicator(activity):
             pixels.mute()
         elif (audiosetup=='AIY'):
             led.ChangeDutyCycle(100)
+        elif (audiosetup=='RUM'):
+            pixel_ring.mono(0xFF0000)
