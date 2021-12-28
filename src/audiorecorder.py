97fd82b0cd79a5daabb41b84dadd8ec5b97accfe
aandroide <djenzo147@gmail.com>
2021-12-28 14:14:50 +0100

    Update audiorecorder.py
    In normal state (100 <= max_value <= 300), then it records the current input waveform.
    E.g. in this program, if the current state is normal, then it returns 0.
    If the max_value of the current input exceeds 300, moving average method is activated, if the current rms_value is less than 300, it returns 0, e.g. normal state.
    If not, it represent third state and return 1.
    If program detects silence state, it increases the num_silent value.
    If the num_silent value is equal to 200, it stops input and terminate. Also saves wav file.
    Terminate condition (num_silent) also can be adjustable.
    The last length parameter represents the length of current input buffer. You can adjust its value you want.

--- src/audiorecorder.py
+++ src/audiorecorder.py
@@ -1,18 +1,42 @@
+#from matplotlib import pyplot as plt
+#import numpy as np
 from sys import byteorder
 from array import array
 from struct import pack
-
+import audioop
 import pyaudio
 import wave
 
-THRESHOLD = 100 #variable adjustment between silence and speech
+
+THRESHOLD = 1000
 CHUNK_SIZE = 4096
 FORMAT = pyaudio.paInt16
 RATE = 44100
 
+
+def check_validity(snd_data):
+    """
+    Returns 0 if the mean value of snd_data is between
+    100 and 300, 
+    If the value is smaller than 100, it returns -1
+    else cases, it returns +1
+    """
+    rms_value = audioop.rms(snd_data, 2)
+    if 98 <= max(snd_data) <= 300:
+        return 0
+    elif max(snd_data) > 300:
+        # Apply moving average method
+        if rms_value < 300:
+            return 0
+        return 1
+    else:
+        return -1
+
+
 def is_silent(snd_data):
     "Returns 'True' if below the 'silent' threshold"
     return max(snd_data) < THRESHOLD
+
 
 def normalize(snd_data):
     "Average the volume out"
@@ -24,6 +48,7 @@ def normalize(snd_data):
         r.append(int(i*times))
     return r
 
+
 def trim(snd_data):
     "Trim the blank spots at the start and end"
     def _trim(snd_data):
@@ -31,7 +56,7 @@ def trim(snd_data):
         r = array('h')
 
         for i in snd_data:
-            if not snd_started and abs(i)>THRESHOLD:
+            if not snd_started and abs(i) > THRESHOLD:
                 snd_started = True
                 r.append(i)
 
@@ -48,12 +73,14 @@ def trim(snd_data):
     snd_data.reverse()
     return snd_data
 
+
 def add_silence(snd_data, seconds):
     "Add silence to the start and end of 'snd_data' of length 'seconds' (float)"
     r = array('h', [0 for i in range(int(seconds*RATE))])
     r.extend(snd_data)
     r.extend([0 for i in range(int(seconds*RATE))])
     return r
+
 
 def record():
     """
@@ -64,34 +91,49 @@ def record():
     blank sound to make sure VLC et al can play
     it without getting chopped off.
     """
+
     p = pyaudio.PyAudio()
-    stream = p.open(format=FORMAT, channels=1, rate=RATE,
-        input=True, output=True,
-        frames_per_buffer=CHUNK_SIZE)
+    stream = p.open(format=FORMAT, 
+                    channels=1, rate=RATE,
+                    input=True, output=True,
+                    frames_per_buffer=CHUNK_SIZE)
 
     num_silent = 0
     snd_started = False
-
     r = array('h')
+
+    # Plot wave setting
+   # _, ax = plt.subplots(figsize=(14, 6))
+   # x = np.arange(0, 2 * CHUNK_SIZE, 2)
+   # ax.set_ylim(-500, 500)
+   # ax.set_xlim(0, CHUNK_SIZE)  # make sure our x axis matched our chunk size
+   # line, = ax.plot(x, np.random.rand(CHUNK_SIZE))
 
     while 1:
         # little endian, signed short
         snd_data = array('h', stream.read(CHUNK_SIZE))
         if byteorder == 'big':
             snd_data.byteswap()
-        r.extend(snd_data)
 
-        silent = is_silent(snd_data)
-        print( [ "NOISE", "SILENCE"][silent], max(snd_data)) # useful to have a print to adjust THRESHOLD based on how your microphone listens
+        # Plot data
+      #  data = np.asarray(snd_data, np.int16)
+      #  line.set_ydata(data)
+      #  plt.pause(0.01)
 
+        print("Testing ===> num_silent:{},\tmax_value:{}, \tlength:{}".format(
+            num_silent, max(snd_data), len(snd_data)))
 
-        if silent and snd_started:
-             num_silent += 1
-        elif not silent and not snd_started:
-             snd_started = True
-        
-        if snd_started and num_silent > 30:
-             break
+        result = check_validity(snd_data)
+        if result == 0:
+            snd_started = True
+            r.extend(snd_data)
+        elif result == 1:
+            continue
+        else:
+            if snd_started:
+                num_silent += 1
+        if snd_started and num_silent > 200:
+            break
 
     sample_width = p.get_sample_size(FORMAT)
     stream.stop_stream()
@@ -102,6 +144,7 @@ def record():
     r = trim(r)
     r = add_silence(r, 0.5)
     return sample_width, r
+
 
 def record_to_file(path):
     "Records from the microphone and outputs the resulting data to 'path'"
@@ -114,6 +157,8 @@ def record_to_file(path):
     wf.setframerate(RATE)
     wf.writeframes(data)
     wf.close()
+    return True
+
 
 if __name__ == '__main__':
     print("please speak a word into the microphone")
